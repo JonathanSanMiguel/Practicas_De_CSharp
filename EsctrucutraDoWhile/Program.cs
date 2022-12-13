bc213db0a8c8f9c737a43d8dc8092524b6c8fa8f
Jonathan San Miguel <guerreroz80@outlook.es>
2022-12-12 20:21:01 -0600

    mas tipos de iteraciones

---
+++ EsctrucutraDoWhile/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace EsctrucutraDoWhile
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int Opcion;
+            do 
+            {
+                Console.WriteLine("Desea seguir viendo este mensaje?");
+                Console.WriteLine("1.Si   2.No");
+                Opcion = Convert.ToInt32(Console.ReadLine());
+
+            } while (Opcion == 1);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EstructuraWhile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace EstructuraWhile
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int Correlactivo;
+
+            Correlactivo = 0;
+            Console.WriteLine("La tabla #5");
+
+            while ( Correlactivo <= 12 )
+            {
+                Console.WriteLine("5 x " + Correlactivo + " = " + (5 * Correlactivo));
+                Correlactivo++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
